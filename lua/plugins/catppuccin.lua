d132ddf535874a6e22877cd80e57fc5a3bf80d9b
mouhamsylla <syllamouhamed99@gmail.com>
2024-05-11 21:43:05 +0000

    tokyonight theme added

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,10 @@
-return { 
-  "catppuccin/nvim",
-  lazy = false,
-  name = "catppuccin", 
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin-mocha"
-  end
+return {
+	"catppuccin/nvim",
+--	lazy = false,
+--	name = "catppuccin",
+--	priority = 1000,
+--	config = function()
+--		vim.cmd.colorscheme("catppuccin-mocha")
+--	end,
+	enable = false,
 }
